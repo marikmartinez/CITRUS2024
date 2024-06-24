11b26f1c39f505c3e337b4f5b614519197861604
marikmartinez <mk727@hawaii.edu>
2024-06-23 15:00:26 -1000

    Set up akmodel, haven't tested on server

--- classifierAI.py
+++ classifierAI.py
@@ -3,9 +3,10 @@ import tensorflow as tf
 import autokeras as ak
 import PIL
 import numpy as np
-import tifffile as tif
+import tifffile as tiff
 import json
 import matplotlib.pyplot as plt
+import utils
 
 class classifierAI:
     def __init__(self): # constructor for the class
@@ -30,54 +31,97 @@ class classifierAI:
 
 class autokerasClassifierAI(classifierAI):
     def __init__(self):
-         self.model = ak.ImageClassifier()
-         self.allImagesList = []
-
-         self.imageTrainList = []
-         self.imageEvalList = []
-
+         self.model = ak.ImageRegressor(output_dim=None, max_trials=3)
+         self.imagelist = []
          self.annotationList = []
-         self.predictionList = []
 
-    def getAllImagesList(self):
-        return self.allImagesList
+    def imagesList(self):
+        return self.imagesList
+    
+    def loadSpecificTreeRGB(self, treeName):
+        treefolder = utils.upperImagesPath + "imagery-" + treeName + os.sep
+        treeImageData = []
+        for imageFolder in os.listdir(treefolder):
+            imageFolder = treefolder + imageFolder
+            for tifImage in os.listdir(imageFolder):
+                if tifImage[-4:] == "tiff":
+                    currImage = tiff.imread(imageFolder + os.sep + tifImage)
+                    currImageArray = np.array(currImage)
+                    # make empty newImage array
+                    newImage = np.zeros(currImageArray.shape)
 
-    def getImageTrainList(self):
-        return self.imageTrainList
+                    # add RGB bands to images
+                    newImage[:, :, 0] = currImageArray[:, :, 1]
+                    newImage[:, :, 1] = currImageArray[:, :, 2]
+                    newImage[:, :, 2] = currImageArray[:, :, 3]
+                    print(tifImage)
+                    currImageArray = np.array(currImage)
+                    treeImageData.append(currImageArray)
+        return np.array(treeImageData)
+    
+    def loadSplitImages(self):
+        treeTypes = ['Abies_alba',
+            'Abies_nordmanniana',
+            'Castanea_sativa',
+            'Fagus_sylvatica',
+            'Larix_decidua',
+            'Picea_abies',
+            'Pinus_halepensis', 
+            'Pinus_nigra', 
+            'Pinus_nigra_laricio', 
+            'Pinus_pinaster', 
+            'Pinus_sylvestris', 
+            'Pseudotsuga_menziesii', 
+            'Quercus_ilex', 
+            'Quercus_petraea', 
+            'Quercus_pubescens',
+            'Quercus_robur',
+            'Quercus_robur',
+            'Quercus_rubra',
+            'Robinia_pseudoacacia']
+        
+        tempAnnotation = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        count = 0
+        treeTypes = [treeTypes[0]]
+        for treeName in treeTypes:
+            # loads in the imagesas a numpy array
+            currTreeList = self.loadSpecificTreeRGB(treeName)
+            
+            currAnnotation = tempAnnotation
+            currAnnotation[count] = 1
+            count += 1
 
-    def getImageEvalList(self):
-        return self.imageEvalList
+            # add curr annotation to annotationslist currTreeList.size() times
+            for n in range(len(currTreeList)):
+                self.annotationList.append(currAnnotation)            
+            self.imagelist.extend(currTreeList)
 
-    # loads images we would like to evaluate on
-    def loadImagesToPredict(self, imagesToAdd):
-        self.predictionList = self.loadImages(imagesToAdd)
 
-    def loadSplitImages(self, trainNames, evalNames):
-        allImageList = []
-        allImageAnnotationList = []
+    # train the model on your old_data
+    def train(self):
+        annotationArray = np.array(self.annotationList)
+        imageArray = np.array(self.imagelist)
+        self.model.fit(imageArray, annotationArray, epochs=1)
 
-        imageTrainList = []
-        imageTrainAnnotationList = []
+    def predict(self):
+        prediction = self.model.predict(self.predictionList)
+        print(prediction)
 
-        imageEvalList = []
-        imageEvalAnnotationList = []
 
-        for imagePath in trainNames:
-            assert(os.path.exists("old_data" + os.sep + "trainImages" + os.sep + "200m" + os.sep + imageName))
-            # loads in the image as a numpy array
 
-            currImage = tif.imread("old_data" + os.sep + "trainImages" + os.sep + "200m" + os.sep + imageName)
-            currImage_array = np.array(currImage)
 
-            imageWidth, imageHeight, imageChannels = currImage_array.shape
+'''
+            currImageArray = np.array(currImage)
+
+            imageWidth, imageHeight, imageChannels = currImageArray.shape
 
             # make empty newImage array
             newImage = np.zeros((imageWidth, imageHeight, 3))
 
             # add RGB bands to images
-            newImage[:, :, 0] = currImage_array[:, :, 2]
-            newImage[:, :, 1] = currImage_array[:, :, 1]
-            newImage[:, :, 2] = currImage_array[:, :, 0]
+            newImage[:, :, 0] = currImageArray[:, :, 1]
+            newImage[:, :, 1] = currImageArray[:, :, 2]
+            newImage[:, :, 2] = currImageArray[:, :, 3]
 
             imageTrainList.append(newImage)
             allImageAnnotationList.append()
@@ -90,17 +134,17 @@ class autokerasClassifierAI(classifierAI):
 
             currImage = tif.imread("old_data" + os.sep + "trainImages" + os.sep + "200m" + os.sep + imageName)
             print('after tif.imread')
-            currImage_array = np.array(currImage)
+            currImageArray = np.array(currImage)
 
-            imageWidth, imageHeight, imageChannels = currImage_array.shape
+            imageWidth, imageHeight, imageChannels = currImageArray.shape
 
             # make empty newImage array
             newImage = np.zeros((imageWidth, imageHeight, 3))
 
             # add RGB bands to images
-            newImage[:, :, 0] = currImage_array[:, :, 2]
-            newImage[:, :, 1] = currImage_array[:, :, 1]
-            newImage[:, :, 2] = currImage_array[:, :, 0]
+            newImage[:, :, 0] = currImageArray[:, :, 2]
+            newImage[:, :, 1] = currImageArray[:, :, 1]
+            newImage[:, :, 2] = currImageArray[:, :, 0]
 
             imageEvalList.append(newImage)
             allImageList.append(newImage)
@@ -108,18 +152,4 @@ class autokerasClassifierAI(classifierAI):
         self.allImagesList = allImageList
         self.imageTrainList = imageTrainList
         self.imageEvalList = imageEvalList
-
-    def loadTrainAnnotations(self, imagesToAdd, annotationsToAdd):
-        for annotationPath in annotationsToAdd:
-            assert(os.path.exists("old_data" + os.sep + "annotations" + os.sep + annotationPath))
-            currAnnotation = np.load("old_data" + os.sep + "annotations" + os.sep + annotationPath)
-
-
-    # train the model on your old_data
-    def train(self):
-        self.model.fit(self.imageList, self.annotationList)
-
-    def predict(self):
-        prediction = self.model.predict(self.predictionList)
-        print(prediction)
-
+'''
--- main.py
+++ main.py
@@ -1,103 +1,88 @@
-#import classifierAI
+import classifierAI
 import utils
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
+
+
+machine_learn = True
+
+
+
 if __name__ == '__main__':
-    #akClassifier = classifierAI.autokerasClassifierAI()
+    if machine_learn:
+        akClassifier = classifierAI.autokerasClassifierAI()
+        akClassifier.loadSplitImages()
+        akClassifier.train()
 
-    # allNames = open('old_data\\s2names.txt').read().splitlines()
-    # trainNames = open('old_data\\trainImages.txt').read().splitlines()
-    # evalNames = open('old_data\\evalImages.txt').read().splitlines()
+        #print(allImageNames)
+    else:
+        treeTypes = ['Abies_alba',
+                'Abies_nordmannaniana',
+                'Castanea_sativa',
+                'Fagus_sylvatica',
+                'Larix_decidua',
+                'Picea_abies',
+                'Pinus_halepensis', 
+                'Pinus_nigra', 
+                'Pinus_nigra_laricio', 
+                'Pinus_pinaster', 
+                'Pinus_sylvestris', 
+                'Pseudotsuga_menziesii', 
+                'Quercus_ilex', 
+                'Quercus_petraea', 
+                'Quercus_pubescens',
+                'Quercus_robur',
+                'Quercus_robur',
+                'Quercus_rubra',
+                'Robinia_pseudoacacia']
+            
+        #load tree data
+        '''
+        for treeName in treeTypes:
+            treeData = utils.loadSpecificTreeImages(treeName)
+        '''    
+        # LOADS ALL IMAGE DATA
 
-    #allImageNames, trainImageNames, evalImageNames = utils.splitData("old_data\\s2names.txt")
-    #allImageNames = open('old_data' + os.sep + 's2names.txt').read().splitlines()
-    #print(len(allImageNames), len(trainImageNames), len(evalImageNames))
-    # print(trainNames)
-    #print(type(trainNames), type(evalNames))
+        for tree in treeTypes:
+            np_allImageData = utils.loadSpecificTreeImages(treeTypes[0])
+            #print(type(np_allImageData))
+            # FIND IMAGE BAND AVG
+            np_ImgBandAvg = utils.findImageBandAvg(np_allImageData)
+            #print(np_ImgBandAvg)
 
-    #akClassifier.loadSplitImages(trainNames, evalNames)
+            plotKeys = {"blue": np.mean(np_ImgBandAvg[:, 0]), "green": np.mean(np_ImgBandAvg[:, 1]),
+                        "red": np.mean(np_ImgBandAvg[:, 2]), "NIR": np.mean(np_ImgBandAvg[:, 3])}
+                                        
+            x = list(plotKeys.keys())
+            y = list(plotKeys.values())
 
-    #print(allImageNames)
+            #bar graph ALL BANDS
+            fig = plt.figure(figsize=(10, 5))
+            plt.bar(x, y, width=0.4, color='brown')
 
-    treeTypes = ['Abies_alba',
-            'Abies_nordmannaniana',
-            'Castanea_sativa',
-            'Fagus_sylvatica',
-            'Larix_decidua',
-            'Picea_abies',
-            'Pinus_halepensis', 
-            'Pinus_nigra', 
-            'Pinus_nigra_laricio', 
-            'Pinus_pinaster', 
-            'Pinus_sylvestris', 
-            'Pseudotsuga_menziesii', 
-            'Quercus_ilex', 
-            'Quercus_petraea', 
-            'Quercus_pubescens',
-            'Quercus_robur',
-            'Quercus_robur',
-            'Quercus_rubra',
-            'Robinia_pseudoacacia']
+            plt.title("Average Band Values Per image with " + tree)
+            plt.xlabel("Multispectral Bands")
+            plt.ylabel("Avg Value Per Image")
+            plt.savefig('graphs' + os.sep + tree + 'AllBandAvg.png')
 
-    #load tree data
-    treeData = utils.loadSpecificTreeImages(treeTypes[0])
-    print('band 1:\n')
-    # first array is images, array inside of that is the pixels, the array inside of the pixels is red green and blue and NIR
-    print(treeData[0][:,:,0])
-    print('band 2\n')
-    print(treeData[0][:,:,1])
-    print('band 3:\n')
-    print(treeData[0][:,:,2])
-    print('band 4:\n')
-    print(treeData[0][:,:,3])
-    
-    # LOADS ALL IMAGE DATA
-    # np_allImageData = utils.loadAllImages()
+            #bar graph RGB
 
-    #print(np_allImageData.shape)
-    #print(np_allImageData[0][:,:,0])
-    #print('max' + str(np_allImageData[:,:,0].max()) + 'min' + str(np_allImageData[:,:,0].min()))
-    
-    '''
-    #print(type(np_allImageData))
-    # FIND IMAGE BAND AVG
-    np_ImgBandAvg = utils.findImageBandAvg(np_allImageData)
-    #print(np_ImgBandAvg)
+            #scaledRGB = utils.scaleRGB(y[:3])
+            fig = plt.figure(figsize=(10, 5))
+            plt.bar(x[:3], y[:3], color='blue')
+            plt.title("Average RBG Band Values Per Image with " + tree)
+            plt.xlabel("RBG Bands")
+            plt.ylabel("Avg % reflectance")
+            plt.savefig('graphs' + os.sep + tree + 'RBGBandAvg.png')
 
-    plotKeys = {"blue": np.mean(np_ImgBandAvg[:, 0]), "green": np.mean(np_ImgBandAvg[:, 1]),
-                "red": np.mean(np_ImgBandAvg[:, 2]), "NIR": np.mean(np_ImgBandAvg[:, 3])}
+            #bar graph for trees
+            #avg band values for each tree type
 
-    x = list(plotKeys.keys())
-    y = list(plotKeys.values())
-
-    #bar graph ALL BANDS
-    fig = plt.figure(figsize=(10, 5))
-    plt.bar(x, y, width=0.4, color='brown')
-
-    plt.title("Average Band Values Per Image")
-    plt.xlabel("Multispectral Bands")
-    plt.ylabel("Avg Value Per Image")
-    plt.savefig('allBandAvg.png')
-
-    #bar graph RGB
-
-    #scaledRGB = utils.scaleRGB(y[:3])
-    fig = plt.figure(figsize=(10, 5))
-    plt.bar(x[:3], y[:3], color='blue')
-    plt.title("Average RBG Band Values Per Image")
-    plt.xlabel("RBG Bands")
-    plt.ylabel("Avg % reflectance")
-    plt.savefig('RBGBandAvg.png')
-
-    #bar graph for trees
-    #avg band values for each tree type
-
-    #scatter plot
-    #x is band
-    #y is values
-    '''
+            #scatter plot
+            #x is band
+            #y is values
 
 
 
